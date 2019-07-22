691003b4f23719dbd7a3594dfabfad7afd24525a
Parashar Gawande <parashargawande@gmail.com>
2019-07-22 17:19:35 +0530

    Update updates.py

--- updates.py
+++ updates.py
@@ -10,6 +10,7 @@ def main():
 	os.system("move /Y updates\* .")
 	updateCommand="python main.py"
 	proc = subprocess.Popen('start cmd /k %s' % updateCommand, shell=True)
+	os.system("del updating.py")
 	exit()
 if __name__ == "__main__":
 	main()
