761e96d3781bc3f146526eda390218604f718c32
Parashar Gawande <parashargawande@gmail.com>
2019-07-22 15:38:32 +0530

    Update updates.py

--- updates.py
+++ updates.py
@@ -6,5 +6,8 @@ def main():
 	os.system("mkdir updates")
 	Repo.clone_from("https://github.com/parashargawande/RemoteControl.git", "updates")
 	os.system("move /Y updates\* .")
+	updateCommand="python main.py"
+	proc = subprocess.Popen('start cmd /k %s' % updateCommand, shell=True)
+	exit()
 if __name__ == "__main__":
 	main()
